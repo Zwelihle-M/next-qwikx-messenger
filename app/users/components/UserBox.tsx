090643c0c815bed9c1246d9474747e500a14f55c
Zwelihle <zwelihle408@gmail.com>
2023-07-17 20:08:32 +0200

    loading states for user feedback

--- app/users/components/UserBox.tsx
+++ app/users/components/UserBox.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Avatar from "@/app/components/Avatar";
+import LoadingModal from "@/app/components/LoadingModal";
 import { User } from "@prisma/client";
 import axios from "axios";
 import { useRouter } from "next/navigation";
@@ -28,20 +29,24 @@ const UserBox: React.FC<UserBoxProps> = ({ data }) => {
   }, [data, router]);
 
   return (
-    <div
-      className="text-white w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
-      onClick={handleClick}
-    >
-      <Avatar user={data} />
+    <>
+      {isLoading && (<LoadingModal />)}
 
-      <div className="min-w-0 flex-1">
-        <div className="focus:outline-none">
-          <div className="flex justify-between items-center mb-1">
-            <p className="text-sm text-black">{data.name}</p>
+      <div
+        className="text-white w-full relative flex items-center space-x-3 bg-white p-3 hover:bg-neutral-100 rounded-lg transition cursor-pointer"
+        onClick={handleClick}
+      >
+        <Avatar user={data} />
+
+        <div className="min-w-0 flex-1">
+          <div className="focus:outline-none">
+            <div className="flex justify-between items-center mb-1">
+              <p className="text-sm text-black">{data.name}</p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
